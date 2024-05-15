410a4582e21c7d404868a9710f87a4aa531d6258
saharshpatel24 <srpatel56@myseneca.ca>
2024-05-15 14:38:26 -0400

    updated code for the logic to handle the status

--- internal/controller/clusterscan_controller.go
+++ internal/controller/clusterscan_controller.go
@@ -24,6 +24,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -37,8 +38,154 @@ type ClusterScanReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+type JobHandleResult struct {
+	Result ctrl.Result
+	Error  error
+	Status string
+}
+
 func newInt32(val int32) *int32 {
 	return &val
+}
+
+func (r *ClusterScanReconciler) handleJob(ctx context.Context, deployment apiv1alpha1.DeploymentSpec) JobHandleResult {
+	// Define the PodSpec
+	log := log.FromContext(ctx)
+	jobSpec := corev1.PodSpec{
+		Containers: []corev1.Container{
+			{
+				Name:    deployment.JobTemplate.Name,
+				Image:   deployment.JobTemplate.Image,
+				Command: deployment.JobTemplate.Command,
+				Args:    deployment.JobTemplate.Args,
+			},
+		},
+		RestartPolicy: corev1.RestartPolicy(deployment.JobTemplate.RestartPolicy),
+	}
+
+	var job batchv1.Job
+	jobName := types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}
+	jobExists := true
+	if err := r.Get(ctx, jobName, &job); err != nil {
+		if errors.IsNotFound(err) {
+			jobExists = false
+		} else {
+			log.Error(err, "unable to fetch CronJob")
+			return JobHandleResult{
+				Result: ctrl.Result{},
+				Error:  err,
+				Status: "Unknow Error",
+			}
+		}
+	}
+
+	if !jobExists {
+		job := &batchv1.Job{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      deployment.Name,
+				Namespace: deployment.Namespace,
+			},
+			Spec: batchv1.JobSpec{
+				Template: corev1.PodTemplateSpec{
+					Spec: jobSpec,
+				},
+			},
+		}
+		if err := r.Create(ctx, job); err != nil {
+			log.Error(err, "unable to create Job for ClusterScan", "Job", job)
+			return JobHandleResult{
+				Result: ctrl.Result{},
+				Error:  err,
+				Status: "Failed creating job",
+			}
+		}
+
+		return JobHandleResult{
+			Result: ctrl.Result{},
+			Error:  nil,
+			Status: "Successfully Deployed",
+		}
+	}
+
+	return JobHandleResult{
+		Result: ctrl.Result{},
+		Error:  nil,
+		Status: "Currently Job is running",
+	}
+}
+
+func (r *ClusterScanReconciler) handleCronJob(ctx context.Context, deployment apiv1alpha1.DeploymentSpec) JobHandleResult {
+	// Fetch the CronJob
+	log := log.FromContext(ctx)
+	jobSpec := corev1.PodSpec{
+		Containers: []corev1.Container{
+			{
+				Name:    deployment.JobTemplate.Name,
+				Image:   deployment.JobTemplate.Image,
+				Command: deployment.JobTemplate.Command,
+				Args:    deployment.JobTemplate.Args,
+			},
+		},
+		RestartPolicy: corev1.RestartPolicy(deployment.JobTemplate.RestartPolicy),
+	}
+
+	var cronJob batchv1.CronJob
+	cronJobName := types.NamespacedName{Name: deployment.Name, Namespace: deployment.Namespace}
+	cronJobExists := true
+	if err := r.Get(ctx, cronJobName, &cronJob); err != nil {
+		if errors.IsNotFound(err) {
+			cronJobExists = false
+		} else {
+			log.Error(err, "unable to fetch CronJob")
+			return JobHandleResult{
+				Result: ctrl.Result{},
+				Error:  err,
+				Status: "Unknow Error",
+			}
+		}
+	}
+
+	// If the CronJob doesn't exist, create it
+	if !cronJobExists {
+		cronJob := &batchv1.CronJob{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      deployment.Name,
+				Namespace: deployment.Namespace,
+			},
+			Spec: batchv1.CronJobSpec{
+				Schedule: deployment.Schedule,
+				JobTemplate: batchv1.JobTemplateSpec{
+					Spec: batchv1.JobSpec{
+						Template: corev1.PodTemplateSpec{
+							Spec: jobSpec,
+						},
+					},
+				},
+				SuccessfulJobsHistoryLimit: newInt32(2),
+				FailedJobsHistoryLimit:     newInt32(1),
+			},
+		}
+		if err := r.Create(ctx, cronJob); err != nil {
+			log.Error(err, "unable to create CronJob for ClusterScan", "CronJob", cronJob)
+			return JobHandleResult{
+				Result: ctrl.Result{},
+				Error:  err,
+				Status: "Failed creating job",
+			}
+		}
+
+		return JobHandleResult{
+			Result: ctrl.Result{},
+			Error:  nil,
+			Status: "Successfully Deployed",
+		}
+	}
+
+	return JobHandleResult{
+		Result: ctrl.Result{},
+		Error:  nil,
+		Status: "Currently CronJob Successfully Running",
+	}
 }
 
 //+kubebuilder:rbac:groups=api.my.domain,resources=clusterscans,verbs=get;list;watch;create;update;patch;delete
@@ -66,73 +213,34 @@ func (r *ClusterScanReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 		}
 		return ctrl.Result{}, err
 	}
-
+	clusterScan.Status.ToolStatuses = nil
 	for _, deployment := range clusterScan.Spec.Deployment {
-		jobSpec := corev1.PodSpec{
-			Containers: []corev1.Container{
-				{
-					Name:    deployment.JobTemplate.Name,
-					Image:   deployment.JobTemplate.Image,
-					Command: deployment.JobTemplate.Command,
-					Args:    deployment.JobTemplate.Args,
-				},
-			},
-			RestartPolicy: corev1.RestartPolicy(deployment.JobTemplate.RestartPolicy),
-		}
-
-		toolStatus := apiv1alpha1.ToolStatus{
-			Name: deployment.Name,
-		}
+		var result JobHandleResult
 
 		if deployment.Reccuring {
-			cronJob := &batchv1.CronJob{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      deployment.Name,
-					Namespace: deployment.Namespace,
-				},
-				Spec: batchv1.CronJobSpec{
-					Schedule: deployment.Schedule,
-					JobTemplate: batchv1.JobTemplateSpec{
-						Spec: batchv1.JobSpec{
-							Template: corev1.PodTemplateSpec{
-								Spec: jobSpec,
-							},
-						},
-					},
-					SuccessfulJobsHistoryLimit: newInt32(2),
-					FailedJobsHistoryLimit:     newInt32(1),
-				},
-			}
-			if err := r.Create(ctx, cronJob); err != nil {
-				log.Error(err, "unable to create CronJob for ClusterScan", "CronJob", cronJob)
-				toolStatus.Status = "Failed"
-				return ctrl.Result{}, err
-			}
+			result = r.handleCronJob(ctx, deployment)
 		} else {
-			job := &batchv1.Job{
-				ObjectMeta: metav1.ObjectMeta{
-					Name:      deployment.Name,
-					Namespace: deployment.Namespace,
-				},
-				Spec: batchv1.JobSpec{
-					Template: corev1.PodTemplateSpec{
-						Spec: jobSpec,
-					},
-				},
-			}
-			if err := r.Create(ctx, job); err != nil {
-				log.Error(err, "unable to create Job for ClusterScan", "Job", job)
-				toolStatus.Status = "Failed"
-				return ctrl.Result{}, err
-			}
+			result = r.handleJob(ctx, deployment)
 		}
 
-		toolStatus.Status = "Deployed"
+		if result.Error != nil {
+			log.Error(result.Error, "error handling job or cronjob")
+			return result.Result, result.Error
+		}
 
-		// Handling Status update
-		clusterScan.Status.ToolStatuses = append(clusterScan.Status.ToolStatuses, toolStatus)
+		toolstatus := apiv1alpha1.ToolStatus{
+			Name:   deployment.JobTemplate.Name,
+			Status: result.Status,
+		}
+
+		// Update the status of the custom resource
+		clusterScan.Status.ToolStatuses = append(clusterScan.Status.ToolStatuses, toolstatus)
 	}
 
+	if err := r.Status().Update(ctx, clusterScan); err != nil {
+		log.Error(err, "unable to update ClusterScan status")
+		return ctrl.Result{}, err
+	}
 	return ctrl.Result{}, nil
 }
 
