f3c70cc4f28c0de226a6b6b9e057be9e3e0c3f26
saharshpatel24 <srpatel56@myseneca.ca>
2024-05-15 12:30:20 -0400

    created logic to create job and cronjob specified in custom resource

--- internal/controller/clusterscan_controller.go
+++ internal/controller/clusterscan_controller.go
@@ -19,6 +19,10 @@ package controller
 import (
 	"context"
 
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -31,6 +35,10 @@ import (
 type ClusterScanReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+}
+
+func newInt32(val int32) *int32 {
+	return &val
 }
 
 //+kubebuilder:rbac:groups=api.my.domain,resources=clusterscans,verbs=get;list;watch;create;update;patch;delete
@@ -47,9 +55,83 @@ type ClusterScanReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *ClusterScanReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	clusterScan := &apiv1alpha1.ClusterScan{}
+	err := r.Get(ctx, req.NamespacedName, clusterScan)
+	if err != nil {
+		log.Error(err, "unable to fetch ClusterScan")
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	for _, deployment := range clusterScan.Spec.Deployment {
+		jobSpec := corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    deployment.JobTemplate.Name,
+					Image:   deployment.JobTemplate.Image,
+					Command: deployment.JobTemplate.Command,
+					Args:    deployment.JobTemplate.Args,
+				},
+			},
+			RestartPolicy: corev1.RestartPolicy(deployment.JobTemplate.RestartPolicy),
+		}
+
+		toolStatus := apiv1alpha1.ToolStatus{
+			Name: deployment.Name,
+		}
+
+		if deployment.Reccuring {
+			cronJob := &batchv1.CronJob{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      deployment.Name,
+					Namespace: deployment.Namespace,
+				},
+				Spec: batchv1.CronJobSpec{
+					Schedule: deployment.Schedule,
+					JobTemplate: batchv1.JobTemplateSpec{
+						Spec: batchv1.JobSpec{
+							Template: corev1.PodTemplateSpec{
+								Spec: jobSpec,
+							},
+						},
+					},
+					SuccessfulJobsHistoryLimit: newInt32(2),
+					FailedJobsHistoryLimit:     newInt32(1),
+				},
+			}
+			if err := r.Create(ctx, cronJob); err != nil {
+				log.Error(err, "unable to create CronJob for ClusterScan", "CronJob", cronJob)
+				toolStatus.Status = "Failed"
+				return ctrl.Result{}, err
+			}
+		} else {
+			job := &batchv1.Job{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      deployment.Name,
+					Namespace: deployment.Namespace,
+				},
+				Spec: batchv1.JobSpec{
+					Template: corev1.PodTemplateSpec{
+						Spec: jobSpec,
+					},
+				},
+			}
+			if err := r.Create(ctx, job); err != nil {
+				log.Error(err, "unable to create Job for ClusterScan", "Job", job)
+				toolStatus.Status = "Failed"
+				return ctrl.Result{}, err
+			}
+		}
+
+		toolStatus.Status = "Deployed"
+
+		// Handling Status update
+		clusterScan.Status.ToolStatuses = append(clusterScan.Status.ToolStatuses, toolStatus)
+	}
 
 	return ctrl.Result{}, nil
 }
